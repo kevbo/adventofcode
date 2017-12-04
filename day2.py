53fc1370e8eb57842790c8ee5c4523ead8593b77
Kevin Boers <kboers@leapfrogonline.com>
2017-12-04 14:10:34 -0600

    Day 2, part 2

--- day2.py
+++ day2.py
@@ -1,10 +1,22 @@
 import unittest
+from itertools import permutations
 
 
 def checksum(matrix):
     total = 0
     for row in matrix:
         total += (max(row) - min(row))
+    return total
+
+
+def checksum2(matrix):
+    total = 0
+    for row in matrix:
+        pairs = permutations(row, 2)
+        for pair in pairs:
+            if not pair[0] == pair[1]:
+                if pair[0] % pair[1] == 0:
+                    total += (pair[0] / pair[1])
     return total
 
 
@@ -19,6 +31,7 @@ def main():
     with open('day2_input.txt') as f:
         matrix = create_matrix(f.readlines())
         print(checksum(matrix))
+        print(checksum2(matrix))
 
 
 class TestChecksum(unittest.TestCase):
@@ -28,6 +41,13 @@ class TestChecksum(unittest.TestCase):
                 [7, 5, 3],
                 [2, 4, 6, 8]]
         self.assertEqual(checksum(test_data), 18)
+
+    def test_checksum2(self):
+        test_data = [
+                [5, 9, 2, 8],
+                [9, 4, 7, 3],
+                [3, 8, 6, 5]]
+        self.assertEqual(checksum2(test_data), 9)
 
 
 if __name__ == '__main__':
