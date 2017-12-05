d5d57940db287566da313895e77b65256b9cf3f3
Kevin Boers <kboers@leapfrogonline.com>
2017-12-05 16:59:54 -0600

    Day 5, part 2

--- day5.py
+++ day5.py
@@ -9,17 +9,23 @@ def load_offsets(filename):
 
 class OffsetHandler(object):
 
-    def __init__(self, offsets):
+    def __init__(self, offsets, decrease_over_three=False):
         self.offsets = offsets
         self.steps = 0
         self.current_index = 0
         self.next_index = 0
+        self.decrease_over_three = decrease_over_three
 
     def run(self):
         while True:
             try:
-                self.next_index = self.current_index + self.offsets[self.current_index]
-                self.offsets[self.current_index] += 1
+                self.next_index = (self.current_index +
+                                   self.offsets[self.current_index])
+                if (self.decrease_over_three and
+                        self.offsets[self.current_index] >= 3):
+                        self.offsets[self.current_index] -= 1
+                else:
+                    self.offsets[self.current_index] += 1
                 self.current_index = self.next_index
                 self.steps += 1
             except IndexError:
@@ -28,19 +34,28 @@ class OffsetHandler(object):
 
 class TestOffsetHandler(unittest.TestCase):
 
-    def setUp(self):
-        self.offsets = [0, 3, 0, 1, -3]
-
-    def test_offset_jumps(self):
+    def test_standard_offsets(self):
         offsets = [0, 3, 0, 1, -3]
         o = OffsetHandler(offsets)
         o.run()
         self.assertEqual(o.offsets, [2, 5, 0, 1, -2])
         self.assertEqual(o.steps, 5)
 
+    def test_offsets_with_decreases(self):
+        offsets = [0, 3, 0, 1, -3]
+        o = OffsetHandler(offsets, decrease_over_three=True)
+        o.run()
+        self.assertEqual(o.offsets, [2, 3, 2, 3, -1])
+        self.assertEqual(o.steps, 10)
+
 
 if __name__ == '__main__':
     # unittest.main()
+    # Part 1
     o = OffsetHandler(load_offsets('day5_input.txt'))
+    o.run()
+    print(o.steps)
+    # Part 2
+    o = OffsetHandler(load_offsets('day5_input.txt'), decrease_over_three=True)
     o.run()
     print(o.steps)
