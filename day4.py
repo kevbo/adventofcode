f4e488f43906f06842b75e96c95a6ef478ce0adb
Kevin Boers <kboers@leapfrogonline.com>
2017-12-05 13:57:09 -0600

    Day 4, part 2

--- day4.py
+++ day4.py
@@ -1,8 +1,18 @@
 import unittest
+from collections import Counter
+from itertools import combinations
 
 
 def is_valid(passphrase):
     return len(passphrase.split()) == len(set(passphrase.split()))
+
+
+def is_valid2(passphrase):
+    combos = combinations(passphrase.split(), 2)
+    for combo in combos:
+        if Counter(combo[0]) == Counter(combo[1]):
+            return False
+    return True
 
 
 class TestValidator(unittest.TestCase):
@@ -13,12 +23,23 @@ class TestValidator(unittest.TestCase):
     def test_invalid(self):
         self.assertFalse(is_valid('aa bb cc dd aa'))
 
+    def test_valid2(self):
+        self.assertTrue(is_valid2('abcde fghij'))
+        self.assertTrue(is_valid2('a ab abc abd abf abj'))
+        self.assertTrue(is_valid2('iiii oiii ooii oooi oooo'))
+
+    def test_invalid2(self):
+        self.assertFalse(is_valid2('abcde xyz ecdab'))
+        self.assertFalse(is_valid2('oiii ioii iioi iiio'))
+
 
 def main():
     with open('day4_input.txt') as f:
         phrases = f.readlines()
     valids = [p for p in phrases if is_valid(p)]
     print(len(valids))
+    valid2s = [p for p in phrases if is_valid2(p)]
+    print(len(valid2s))
 
 
 if __name__ == '__main__':
