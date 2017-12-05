ab2b6dd2225bcbe6b78e00fcdb5f453cdfc12cb3
Kevin Boers <kboers@leapfrogonline.com>
2017-12-05 13:27:58 -0600

    Day 3, part 2

--- day3.py
+++ day3.py
@@ -8,7 +8,8 @@ class Walker(object):
         self.coords = (0, 0)
         self.steps = steps
         self.next_step = 2
-        self.points = {(0, 0): 1}
+        self.points = {(0, 0): {'step': 1, 'value': 1}}
+        self.tabulator = [1]
         for i in range(steps)[1:]:
             if self.can_i_go_left():
                 self.turn_left()
@@ -18,6 +19,9 @@ class Walker(object):
 
     def get_current_distance(self):
         return abs(self.coords[0]) + abs(self.coords[1])
+
+    def get_current_value(self):
+        return self.points[self.coords]['value']
 
     def can_i_go_left(self):
         if self.orientation == 'S':
@@ -43,28 +47,61 @@ class Walker(object):
         elif self.orientation == 'W':
             self.orientation = 'S'
 
+    def sum_value_of_neighbors(self):
+        sum_neighbors = 0
+        # N
+        if (self.coords[0], self.coords[1] + 1) in self.points:
+            sum_neighbors += self.points[(self.coords[0], self.coords[1] + 1)]['value']
+        # E
+        if (self.coords[0] + 1, self.coords[1]) in self.points:
+            sum_neighbors += self.points[(self.coords[0] + 1, self.coords[1])]['value']
+        # S
+        if (self.coords[0], self.coords[1] - 1) in self.points:
+            sum_neighbors += self.points[(self.coords[0], self.coords[1] - 1)]['value']
+        # W
+        if (self.coords[0] - 1, self.coords[1]) in self.points:
+            sum_neighbors += self.points[(self.coords[0] - 1, self.coords[1])]['value']
+        # NE
+        if (self.coords[0] + 1, self.coords[1] + 1) in self.points:
+            sum_neighbors += self.points[(self.coords[0] + 1, self.coords[1] + 1)]['value']
+        # SE
+        if (self.coords[0] + 1, self.coords[1] - 1) in self.points:
+            sum_neighbors += self.points[(self.coords[0] + 1, self.coords[1] - 1)]['value']
+        # SW
+        if (self.coords[0] - 1, self.coords[1] - 1) in self.points:
+            sum_neighbors += self.points[(self.coords[0] - 1, self.coords[1] - 1)]['value']
+        # NW
+        if (self.coords[0] - 1, self.coords[1] + 1) in self.points:
+            sum_neighbors += self.points[(self.coords[0] - 1, self.coords[1] + 1)]['value']
+        return sum_neighbors
+
     def step_forward(self):
         if self.orientation == 'S':
             self.coords = (self.coords[0], self.coords[1] - 1)
-            self.points[self.coords] = self.next_step
+            self.points[self.coords] = {'step': self.next_step, 'value': self.sum_value_of_neighbors()}
             self.next_step += 1
-        if self.orientation == 'E':
+        elif self.orientation == 'E':
             self.coords = (self.coords[0] + 1, self.coords[1])
-            self.points[self.coords] = self.next_step
+            self.points[self.coords] = {'step': self.next_step, 'value': self.sum_value_of_neighbors()}
             self.next_step += 1
-        if self.orientation == 'N':
+        elif self.orientation == 'N':
             self.coords = (self.coords[0], self.coords[1] + 1)
-            self.points[self.coords] = self.next_step
+            self.points[self.coords] = {'step': self.next_step, 'value': self.sum_value_of_neighbors()}
             self.next_step += 1
-        if self.orientation == 'W':
+        elif self.orientation == 'W':
             self.coords = (self.coords[0] - 1, self.coords[1])
-            self.points[self.coords] = self.next_step
+            self.points[self.coords] = {'step': self.next_step, 'value': self.sum_value_of_neighbors()}
             self.next_step += 1
 
 
 def main(steps):
-    w = Walker(312051)
+    w = Walker(steps)
     print(w.get_current_distance())
+    for step in range(steps)[1:]:
+        w = Walker(step)
+        if w.get_current_value() > steps:
+            print('First value bigger than %s: %s' % (steps, w.get_current_value()))
+            break
 
 
 class TestMatrixDistance(unittest.TestCase):
@@ -72,12 +109,17 @@ class TestMatrixDistance(unittest.TestCase):
         w = Walker(1)
         self.assertEqual(w.get_current_distance(), 0)
         w = Walker(12)
-        print(w.points)
         self.assertEqual(w.get_current_distance(), 3)
         w = Walker(23)
         self.assertEqual(w.get_current_distance(), 2)
         w = Walker(1024)
         self.assertEqual(w.get_current_distance(), 31)
+
+    def test_tabulation(self):
+        w = Walker(1)
+        self.assertEqual(w.get_current_value(), 1)
+        w = Walker(12)
+        self.assertEqual(w.get_current_value(), 57)
 
 
 if __name__ == '__main__':
