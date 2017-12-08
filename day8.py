65a387bbd95dd69abbb8207c275908c2d3c78956
Kevin Boers <kboers@leapfrogonline.com>
2017-12-08 16:42:31 -0600

    Day 8, part 1

---
+++ day8.py
@@ -0,0 +1,49 @@
+import re
+from collections import defaultdict
+
+
+class RegisterProcessor(object):
+
+    def __init__(self, instructions):
+        self.registers = defaultdict(int)
+        self.instructions = self.parse_instructions(instructions)
+        for i in self.instructions:
+            self.handle_instruction(i)
+
+    def parse_instructions(self, instructions):
+        out = []
+        instructions = instructions.strip().split('\n')
+        instructions = [i.strip() for i in instructions]
+        for line in instructions:
+            rgx = re.compile('^(\w+) (inc|dec) (-?\d+) if (\w+) (.*) (-?\d+)$')
+            matches = rgx.search(line)
+            out.append(matches.groups())
+        return out
+
+    def handle_instruction(self, instruction):
+        target = instruction[0]
+        operation = instruction[1]
+        num = instruction[2]
+        eval_target = instruction[3]
+        eval_comparator = instruction[4]
+        eval_against = instruction[5]
+        if eval('self.registers["%s"] %s %s' % (eval_target,
+                                                eval_comparator,
+                                                eval_against)):
+            if operation == 'inc':
+                self.registers[target] += int(num)
+            elif operation == 'dec':
+                self.registers[target] -= int(num)
+
+    @property
+    def highest_value(self):
+        max_int = [{k: v} for k, v in self.registers.items()
+                   if v == max(self.registers.values())]
+        return max_int[0]
+
+
+if __name__ == '__main__':  # pragma: no cover
+    with open('day8_input.txt') as f:
+        data = f.read()
+    rp = RegisterProcessor(data)
+    print('Highest value found in any register:', rp.highest_value)
---
+++ tests/test_day8.py
@@ -0,0 +1,21 @@
+import unittest
+
+from day8 import RegisterProcessor
+
+
+class TestRegisterProcessor(unittest.TestCase):
+
+    def setUp(self):
+        self.input = """b inc 5 if a > 1
+        a inc 1 if b < 5
+        c dec -10 if a >= 1
+        c inc -20 if c == 10"""
+        self.rp = RegisterProcessor(self.input)
+
+    def test_final_values(self):
+        self.assertEqual(self.rp.registers['a'], 1)
+        self.assertEqual(self.rp.registers['b'], 0)
+        self.assertEqual(self.rp.registers['c'], -10)
+
+    def test_highest_value(self):
+        self.assertEqual(self.rp.highest_value, {'a': 1})
